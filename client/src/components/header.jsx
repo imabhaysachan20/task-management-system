799fb493cd9bfa96f114db54de585100fd26e4e2
abhay <imabhaysachan@gmail.com>
2025-08-02 00:36:42 +0530

    fix imports

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,14 +1,6 @@
-import { useEffect, useState } from "react";
 import { useAuth } from "@/contexts/AuthContext";
 import { Button } from "@/components/ui/button";
-import { LogOut, Rss, User } from "lucide-react";
-import TaskList from "@/components/TaskList";
-import TaskForm from "@/components/TaskForm";
-import API from "@/lib/axios";
-
-
-
-
+import { LogOut, User } from "lucide-react";
 
 function Header() {
   const { user, logout } = useAuth();
--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,10 +1,4 @@
-import { useEffect, useState } from "react";
 import { useAuth } from "@/contexts/AuthContext";
-import { Button } from "@/components/ui/button";
-import { LogOut, Rss, User } from "lucide-react";
-import TaskList from "@/components/TaskList";
-import TaskForm from "@/components/TaskForm";
-import API from "@/lib/axios";
 import AdminDashboard from "@/components/AdminDashboard";
 import UserDashboard from "@/components/UserDashboard";
 
